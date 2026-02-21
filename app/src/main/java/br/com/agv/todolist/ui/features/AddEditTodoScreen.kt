91a6ed459a7da59a89c17efc2707b3ccf7e2fa09
gabrielcarvalhotp <gabrieltp2004@gmail.com>
2026-02-21 10:30:03 -0300

    create add and edit todo screen

---
+++ app/src/main/java/br/com/agv/todolist/ui/features/AddEditTodoScreen.kt
@@ -0,0 +1,73 @@
+package br.com.agv.todolist.ui.features
+
+import android.widget.Space
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.room.util.TableInfo
+import br.com.agv.myapplication.R
+import br.com.agv.todolist.ui.theme.TodoListTheme
+
+@Composable
+fun AddEditTodoScreen() {
+
+}
+
+@Composable
+fun AddEditTodoContent(modifier: Modifier = Modifier) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { }) {
+                Icon(
+                    painterResource(R.drawable.ic_check_24px),
+                    contentDescription = stringResource(R.string.add)
+                )
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .consumeWindowInsets(paddingValues)
+                .padding(16.dp),
+        ) {
+            OutlinedTextField(
+                value = "",
+                onValueChange = { },
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = { Text(stringResource(R.string.title)) },
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = { },
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = { Text(stringResource(R.string.description)) },
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AddEditTodoContentPreview() {
+    TodoListTheme {
+        AddEditTodoContent()
+    }
+}
